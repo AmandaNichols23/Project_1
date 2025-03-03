fa3168462e6bd49b7b95020cc227cf6defe19007
AmandaNichols23 <anichols291@gmail.com>
2025-03-02 21:39:49 -0500

    Feat
    Added password verification to project1submit.php, created project1error.php, and renamed survey

---
+++ project1error.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html lang="en-US">
+       <head>
+            <meta charset="utf-8">
+         </head>
+          <body>
+            <h1> Something went wrong! </h1>
+            <h2> Make sure your email and password are right, and you answered all the questions!</h2> <!-- This is the error page for if something went wrong in the survey --> 
+            <form action="project1starter.php" method="post" class="survey-form">
+            <button type="submit" name="return-to-survey" id="submit-button">Return To survey</button>
+          </body>
+    </html>
+    </doctype>
--- project1submit.php
+++ project1submit.php
@@ -4,6 +4,26 @@
             <meta charset="utf-8">
          </head>
           <body>
+            <?php
+                $globalPassHash = '$2y$10$FR9ixFJYJyqicuQ8Es6/ZulZJUlVlMD/QWHtcizwdJQMSXVWdTSQq';
+                $userEnteredPassword = $_POST['pw-name'];
+
+                $error = false;
+
+                if (password_verify($userEnteredPassword, $globalPassHash)) { //checks if password is correct before doing anything else, and sets an error flag if password is incorrect
+
+                }
+                else {
+                    $error = true;
+                }
+
+                if($error) //if the error flag was set at any point, redirect the user to project1error.php
+                {
+                    header("Location: project1error.php");
+                }
+            ?>
+            <form action="project1starter.php" method="post" class="survey-form">
+            <button type="submit" name="return-to-survey" id="submit-button">Return To survey</button>
           </body>
     </html>
     </doctype>
